979de39c1d81072298f166c56b7932eb1598d002
Grace Sinor <g.sinor1@saints.mhcc.edu>
2023-05-12 20:48:07 -0700

    Flexbox Module & Module 12

---
+++ module_12/sample_project/fitness-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Grace Sinor
+    File Name: script.js
+    Date: May 11, 2023
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
